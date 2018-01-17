b84bcb82b3df9270c901f615c18deaacf5fa80cc
Archita21 <35433234+Archita21@users.noreply.github.com>
2018-01-18 01:48:55 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,22 @@
+ #include<stdio.h>
+ int main()
+{
+    char n;
+    
+ printf("Enter thr character\n");
+ scanf("%c",&n);
+ 
+ if(n=='a' || n=='e' || n== 'o'|| n== 'u' || n=='i' || n=='A' || n=='E' || n =='O' || n=='U')
+ {
+ printf("vowel");
+}
+ else
+ {
+ printf("consonant");
+}
+ return 0;
+}
+                     
+                     
+                   
+    
